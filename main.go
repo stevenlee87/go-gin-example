e8d1bf08d6b2c32561e22215725ac18c9a157cf7
stevenlee87 <pxnW6tiRfME8akg>
2019-11-18 18:04:29 +0800

    first commit after init!
    127.0.0.1:8000/test

---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/stevenlee87/go-gin-example/pkg/setting"
+	"github.com/stevenlee87/go-gin-example/routers"
+)
+
+func main() {
+	router := routers.InitRouter()
+
+	s := &http.Server{
+		Addr:           fmt.Sprintf(":%d", setting.HTTPPort),
+		Handler:        router,
+		ReadTimeout:    setting.ReadTimeout,
+		WriteTimeout:   setting.WriteTimeout,
+		MaxHeaderBytes: 1 << 20,
+	}
+
+	s.ListenAndServe()
+}
