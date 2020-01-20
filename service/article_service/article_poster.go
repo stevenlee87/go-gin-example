0f398869264629c8ea49337cf0ab277e75803409
stevenlee87 <imepisode1@gmail.com>
2020-01-20 14:27:18 +0800

    edit article_poster: add a comment

--- service/article_service/article_poster.go
+++ service/article_service/article_poster.go
@@ -9,8 +9,6 @@ import (
 
 	"github.com/stevenlee87/go-gin-example/pkg/logging"
 
-	"fmt"
-
 	"github.com/golang/freetype"
 
 	"github.com/stevenlee87/go-gin-example/pkg/file"
@@ -137,10 +135,81 @@ func (a *ArticlePosterBg) DrawPoster(d *DrawText, fontName string) error {
 	return nil
 }
 
+/*
+func (a *ArticlePosterBg) Generate() 方法，做了如下事情：
+获取二维码存储路径
+生成二维码图像
+检查合并后图像（指的是存放合并后的海报）是否存在
+若不存在，则生成待合并的图像 mergedF
+打开事先存放的背景图 bgF
+打开生成的二维码图像 qrF
+解码 bgF 和 qrF 返回 image.Image
+创建一个新的 RGBA 图像
+在 RGBA 图像上绘制 背景图（bgF）
+在已绘制背景图的 RGBA 图像上，在指定 Point 上绘制二维码图像（qrF）
+将绘制好的 RGBA 图像以 JPEG 4：2：0 基线格式写入合并后的图像文件（mergedF）
+*/
+//func (a *ArticlePosterBg) Generate() (string, string, error) {
+//	fullPath := qrcode.GetQrCodeFullPath()
+//	fileName, path, err := a.Qr.Encode(fullPath)
+//	if err != nil {
+//		return "", "", err
+//	}
+//
+//	if !a.CheckMergedImage(path) {
+//		mergedF, err := a.OpenMergedImage(path)
+//		if err != nil {
+//			return "", "", err
+//		}
+//		defer mergedF.Close()
+//
+//		bgF, err := file.MustOpen(a.Name, path)
+//		if err != nil {
+//			return "", "", err
+//		}
+//		defer bgF.Close()
+//
+//		qrF, err := file.MustOpen(fileName, path)
+//		if err != nil {
+//			return "", "", err
+//		}
+//		defer qrF.Close()
+//
+//		bgImage, err := jpeg.Decode(bgF)
+//		if err != nil {
+//			return "", "", err
+//		}
+//		qrImage, err := jpeg.Decode(qrF)
+//		if err != nil {
+//			return "", "", err
+//		}
+//
+//		jpg := image.NewRGBA(image.Rect(a.Rect.X0, a.Rect.Y0, a.Rect.X1, a.Rect.Y1))
+//
+//		draw.Draw(jpg, jpg.Bounds(), bgImage, bgImage.Bounds().Min, draw.Over)
+//		draw.Draw(jpg, jpg.Bounds(), qrImage, qrImage.Bounds().Min.Sub(image.Pt(a.Pt.X, a.Pt.Y)), draw.Over)
+//
+//		jpeg.Encode(mergedF, jpg, nil)
+//	}
+//
+//	return fileName, path, nil
+//}
+
+/*
+image.Rect     			 https://golang.org/pkg/image/#Rect
+image.Pt       			 https://golang.org/pkg/image/#Pt
+image.NewRGBA  			 https://golang.org/pkg/image/#NewRGBA
+jpeg.Encode    			 https://golang.org/pkg/image/jpeg/#Encode
+jpeg.Decode    			 https://golang.org/pkg/image/jpeg/#Decode
+draw.Op        			 https://golang.org/pkg/image/draw/#Op
+draw.Draw      			 https://golang.org/pkg/image/draw/#Draw
+go-imagedraw-package     https://blog.golang.org/go-imagedraw-package
+*/
+
 func (a *ArticlePosterBg) Generate() (string, string, error) {
 	fullPath := qrcode.GetQrCodeFullPath() // runtime/ + qrcode/
 	fileName, path, err := a.Qr.Encode(fullPath)
-	fmt.Printf("fileName is:%s, path is %s", fileName, path)
+	//fmt.Printf("fileName is:%s, path is %s", fileName, path)
 	if err != nil {
 		return "", "", err
 	}
